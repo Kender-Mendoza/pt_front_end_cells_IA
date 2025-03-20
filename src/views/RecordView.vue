ccf14c899a53e2849fff72bf28a9940a01d5131a
Kender <kendermendoza591@gmail.com>
2025-03-20 11:07:40 +0100

    - added code spliting

--- src/views/RecordView.vue
+++ src/views/RecordView.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
-
-import NewRecordComponent from '@/components/records/NewRecordComponent.vue'
+import { defineAsyncComponent, ref } from 'vue';
 import ButtonComponent from '@/components/shared/ButtonComponent.vue'
-import NotificationComponent from '@/components/shared/NotificationComponent.vue'
-import WrapperModalLayout from '@/layouts/WrapperModalLayout.vue'
+
+const NewRecordComponent = defineAsyncComponent(() => import('@/components/records/NewRecordComponent.vue'))
+const NotificationComponent = defineAsyncComponent(() => import('@/components/shared/NotificationComponent.vue'))
+const WrapperModalLayout = defineAsyncComponent(() => import('@/layouts/WrapperModalLayout.vue'))
 
 const { t } = useI18n()
 const showModal = ref<boolean>(false)
@@ -21,10 +21,7 @@ const toggleModal = (openModal: boolean, showNotification: boolean) => {
   <NotificationComponent v-if="notification" :content="t('views.record_view.record_created')" />
 
   <div class="pa-4 text-center">
-    <ButtonComponent
-      :content="t('views.record_view.create_record')"
-      @on-click-button="toggleModal(true, false)"
-    />
+    <ButtonComponent :content="t('views.record_view.create_record')" @on-click-button="toggleModal(true, false)" />
     <WrapperModalLayout :title="t('views.record_view.new_record')" v-model="showModal">
       <NewRecordComponent @close-modal="toggleModal" />
     </WrapperModalLayout>
